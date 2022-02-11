a7ef0f8e68d32eee6db95fa8ffa7d64a45a032ac
9191stc <9191stc@gmail.com>
2022-02-11 17:08:31 +0800

    auto deploy

--- scripts/auto_deploy.sh
+++ scripts/auto_deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+##
 ## 1. local startup a dev/proxima/barnard node
 ## 2. Copy ipc file path
 ## 3. run this script, pass the ipc file path.
+##
+## eg: ./scripts/auto_deploy.sh /var/folders/yl/1jwdcrx91p96yh8c0yw03x280000gn/T/5d0f92950052e50d78fd7654e10e5481/dev/starcoin.ipc
+##
 IPC=$1
 WORK_DIR=$(pwd)
 
