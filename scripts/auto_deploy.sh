e8d157cfc8391b923b67c330ea852a9124ae2a75
9191stc <9191stc@gmail.com>
2022-02-11 15:33:47 +0800

    auto deploy

--- scripts/auto_deploy.sh
+++ scripts/auto_deploy.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+## 1. local startup a dev/proxima/barnard node
+## 2. Copy ipc file path
+## 3. run this script, pass the ipc file path.
 IPC=$1
 WORK_DIR=$(pwd)
 
 ${WORK_DIR}/scripts/deploy/deploy_util.sh dev $IPC ${WORK_DIR}/scripts/deploy/commands_init_dev.cmd
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
 ${WORK_DIR}/scripts/deploy/deploy_util.sh dev $IPC ${WORK_DIR}/scripts/deploy/commands_init_swap.cmd
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
 ${WORK_DIR}/scripts/deploy/deploy_util.sh dev $IPC ${WORK_DIR}/scripts/deploy/commands_init_syrup.cmd
+if [ $? -ne 0 ]; then
+  exit 1
+fi
