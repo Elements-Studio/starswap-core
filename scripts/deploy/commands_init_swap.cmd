4aede47f247fbb5f6f2f82b74f0f50c04f2fd0e3
9191stc <9191stc@gmail.com>
2022-02-11 18:15:55 +0800

    auto deploy

--- scripts/deploy/commands_init_swap.cmd
+++ scripts/deploy/commands_init_swap.cmd
@@ -5,26 +5,6 @@ account unlock
 ### Swap Fee admin账号 accept XUSDT
 account execute-function --function 0x1::Account::accept_token -t 0x2d81a0427d64ff61b11ede9085efa5ad::XUSDT::XUSDT -s 0x0a4183ac9335a9f5804014eab01c0abc -b
 
-### 治理币创世初始化 
-account execute-function --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapGovScript::genesis_initialize -s 0x4783d08fb16990bd35d83f3e23bf93b8 -b 
-
-### 设置swap operation fee rate
-account execute-function --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapScripts::set_swap_fee_operation_rate --arg 10u64 --arg 60u64 -s 0x4783d08fb16990bd35d83f3e23bf93b8 -b
-
-### 提取STAR
-account execute-function -s 0x4783d08fb16990bd35d83f3e23bf93b8 --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapGovScript::dispatch  -t 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapGovPoolType::PoolTypeDaoCrosshain --arg  0x4783d08fb16990bd35d83f3e23bf93b8  --arg 210000000000000u128  -b
-
-### 管理员创建swap交易对 
-account execute-function --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapScripts::register_swap_pair -t 0x1::STC::STC -t 0x2d81a0427d64ff61b11ede9085efa5ad::XUSDT::XUSDT -s 0x4783d08fb16990bd35d83f3e23bf93b8 -b
-# account execute-function -s 0x4783d08fb16990bd35d83f3e23bf93b8 --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapScripts::register_swap_pair -t  0x1::STC::STC -t 0xfe125d419811297dfab03c61efec0bc9::FAI::FAI -b
-# account execute-function -s 0x4783d08fb16990bd35d83f3e23bf93b8 --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapScripts::register_swap_pair -t 0x4783d08fb16990bd35d83f3e23bf93b8::STAR::STAR -t 0x1::STC::STC -b
-
-### 添加代币对流动性 
-account execute-function -s 0x2d81a0427d64ff61b11ede9085efa5ad --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapScripts::add_liquidity -t 0x1::STC::STC -t 0x2d81a0427d64ff61b11ede9085efa5ad::XUSDT::XUSDT --arg 50000000u128 --arg 25000000u128 --arg 5000u128 --arg 5000u128 -b
-
-### 查询刚刚创建的交易对流动性 
-dev call --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapRouter::total_liquidity -t 0x1::STC::STC -t 0x2d81a0427d64ff61b11ede9085efa5ad::XUSDT::XUSDT
-
 ### 管理员创建Farm 
 account execute-function -s 0x4783d08fb16990bd35d83f3e23bf93b8 --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapFarmScript::add_farm_pool -t 0x1::STC::STC -t 0x2d81a0427d64ff61b11ede9085efa5ad::XUSDT::XUSDT --arg 1000000000u128 -b
 
@@ -45,10 +25,10 @@ dev gen-block
 dev call --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapFarmScript::lookup_gain -t 0x1::STC::STC -t 0x2d81a0427d64ff61b11ede9085efa5ad::XUSDT::XUSDT --arg 0x2d81a0427d64ff61b11ede9085efa5ad
 
 ### 领取奖励 
-account execute-function -s 0x2d81a0427d64ff61b11ede9085efa5ad--function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapFarmScript::harvest -t 0x1::STC::STC -t 0x2d81a0427d64ff61b11ede9085efa5ad::XUSDT::XUSDT-b --arg 0u128 
+account execute-function -s 0x2d81a0427d64ff61b11ede9085efa5ad --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapFarmScript::harvest -t 0x1::STC::STC -t 0x2d81a0427d64ff61b11ede9085efa5ad::XUSDT::XUSDT -b --arg 0u128
 
 ### 查看STAR额度 
 account show 0x2d81a0427d64ff61b11ede9085efa5ad
 
 ### 取出流动性 
-account execute-function -s 0x2d81a0427d64ff61b11ede9085efa5ad--function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapFarmScript::unstake -t 0x1::STC::STC -t 0x2d81a0427d64ff61b11ede9085efa5ad::XUSDT::XUSDT -b --arg 10002u128
+account execute-function -s 0x2d81a0427d64ff61b11ede9085efa5ad --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapFarmScript::unstake -t 0x1::STC::STC -t 0x2d81a0427d64ff61b11ede9085efa5ad::XUSDT::XUSDT -b --arg 10002u128
--- scripts/deploy/commands_init_syrup.cmd
+++ scripts/deploy/commands_init_syrup.cmd
@@ -1,3 +1,5 @@
+### 提取 STAR
+account execute-function -s 0x4783d08fb16990bd35d83f3e23bf93b8 --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapGovScript::dispatch   -t 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapGovPoolType::PoolTypeDaoCrosshain --arg  0x4783d08fb16990bd35d83f3e23bf93b8  --arg 210000000000000u128  -b
 
 ### 管理员创建Syrup
 account execute-function -s 0x4783d08fb16990bd35d83f3e23bf93b8 --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapSyrupScript::add_pool -t 0x4783d08fb16990bd35d83f3e23bf93b8::STAR::STAR --arg 1000000000u128 --arg 0u64 -b
@@ -6,7 +8,7 @@ account execute-function -s 0x4783d08fb16990bd35d83f3e23bf93b8 --function 0x4783
 dev call --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapSyrup::query_release_per_second -t 0x4783d08fb16990bd35d83f3e23bf93b8::STAR::STAR
 
 ### 质押Syrup
-account execute-function -s 0x4783d08fb16990bd35d83f3e23bf93b8 --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapSyrupScript::stake -t 0x4783d08fb16990bd35d83f3e23bf93b8::STAR::STAR --arg 2592000u64 --arg 23000000000u128 -b
+account execute-function -s 0x4783d08fb16990bd35d83f3e23bf93b8 --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapSyrupScript::stake -t 0x4783d08fb16990bd35d83f3e23bf93b8::STAR::STAR --arg 10000u64 --arg 100000000u128 -b
 
 ### 查看stake list
 dev call --function 0x4783d08fb16990bd35d83f3e23bf93b8::TokenSwapSyrupScript::query_stake_list -t 0x4783d08fb16990bd35d83f3e23bf93b8::STAR::STAR --arg 0x4783d08fb16990bd35d83f3e23bf93b8
