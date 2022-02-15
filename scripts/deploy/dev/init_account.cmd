17a8713257fe505ac3c9a98e4830d7f13f863af7
element2050 <vilerhnvrf@gmail.com>
2022-02-15 17:14:56 +0800

    bug fixed

--- scripts/deploy/dev/init_account.cmd
+++ scripts/deploy/dev/init_account.cmd
@@ -1,17 +1,14 @@
 ### bridge合约admin账号
-account import -i 0xf4611222ffb7a0d348596132b94ac63f61e78bea4da87d3d9f9009b2abb4ce82
 account default 0x4c438026f963f52f01f612d1e8c41bc4
 account unlock 0x4c438026f963f52f01f612d1e8c41bc4
 dev get-coin 0x4c438026f963f52f01f612d1e8c41bc4
 
 ### Swap Fee admin账号
-account import -i 0x1f5bfa4af32fe7c0604efba5146e3341153ff8245cc39a1e4000d09727a58f03
 account default  0x0a4183ac9335a9f5804014eab01c0abc
 account unlock 0x0a4183ac9335a9f5804014eab01c0abc
 dev get-coin 0x0a4183ac9335a9f5804014eab01c0abc
 
-### Swap合约admin账号，0x7d50cc0b71d372299d5a3f8aeabc9aa6b911628b6865d9ed78985124633eea37 为老账户
-account import -i 0xb9ac5b4924d01efa8c3e465b5df31416f76907c6deffd8956a5ef508bb87af02
+### Swap合约admin账号，
 account default 0x2b3d5bd6d0f8a957e6a4abe986056ba7
 account unlock 0x2b3d5bd6d0f8a957e6a4abe986056ba7
 dev get-coin 0x2b3d5bd6d0f8a957e6a4abe986056ba7
