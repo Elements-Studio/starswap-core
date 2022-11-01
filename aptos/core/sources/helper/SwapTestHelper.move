8b0dd2f7ed68b72c1c1473172723b2c3e5f38873
element2050 <vilerhnvrf@gmail.com>
2022-11-01 10:58:17 +0800

    resolve merge confilct via code cleaning

--- aptos/core/sources/helper/SwapTestHelper.move
+++ aptos/core/sources/helper/SwapTestHelper.move
@@ -1,12 +1,4 @@
 module SwapAdmin::SwapTestHelper {
-    // use aptos_framework::coin::{Self};
-    // use aptos_framework::aptos_coin::{AptosCoin as APT};
-    // use std::signer;
-
-    // use SwapAdmin::TokenSwapRouter;
-    // use bridge::asset::USDT;
-    // use SwapAdmin::TokenMock::{Self};
-    // use SwapAdmin::TokenMock::{Self, WETH, WUSDT, WDAI, WBTC};
 
     const PRECISION_9: u8 = 9;
     const PRECISION_18: u8 = 18;
@@ -32,41 +24,11 @@ module SwapAdmin::SwapTestHelper {
     }
 
     public fun init_token_pairs_register(_account: &signer) {
-        //  TokenSwapRouter::register_swap_pair<WETH, WUSDT>(account);
-        //  assert!(TokenSwapRouter::swap_pair_exists<WETH, WUSDT>(), 111);
-        //
-        //  TokenSwapRouter::register_swap_pair<WUSDT, WDAI>(account);
-        //  assert!(TokenSwapRouter::swap_pair_exists<WUSDT, WDAI>(), 112);
-        //
-        //  TokenSwapRouter::register_swap_pair<WDAI, WBTC>(account);
-        //  assert!(TokenSwapRouter::swap_pair_exists<WDAI, WBTC>(), 113);
-        //
-        // TokenSwapRouter::register_swap_pair<APT, WETH>(account);
-        // assert!(TokenSwapRouter::swap_pair_exists<APT, WETH>(), 114);
-        //
-        //  TokenSwapRouter::register_swap_pair<WBTC, WETH>(account);
-        //  assert!(TokenSwapRouter::swap_pair_exists<WBTC, WETH>(), 115);
-        //
-        // TokenSwapRouter::register_swap_pair<APT, XUSDT>(account);
-        // assert!(TokenSwapRouter::swap_pair_exists<APT, XUSDT>(), 116);
-        //
-        //  TokenSwapRouter::register_swap_pair<WETH, XUSDT>(account);
-        //  assert!(TokenSwapRouter::swap_pair_exists<WETH, XUSDT>(), 117);
     }
 
     public fun init_token_pairs_liquidity(_account: &signer) {
-        //  TokenSwapRouter::add_liquidity<WETH, WUSDT>(account, 5000, 100000, 100, 100);
-        //  TokenSwapRouter::add_liquidity<WUSDT, WDAI>(account, 20000, 30000, 100, 100);
-        //  TokenSwapRouter::add_liquidity<WDAI, WBTC>(account, 100000, 4000, 100, 100);
-        // TokenSwapRouter::add_liquidity<APT, WETH>(account, 200000, 10000, 100, 100);
-        //  TokenSwapRouter::add_liquidity<WETH, WBTC>(account, 60000, 5000, 100, 100);
-        // TokenSwapRouter::add_liquidity<APT, XUSDT>(account, 160000, 5000, 100, 100);
-        //  TokenSwapRouter::add_liquidity<WETH, XUSDT>(account, 6000, 20000, 100, 100);
     }
 
     public fun init_fee_token(_account: &signer) {
-        // TokenMock::register_token<XUSDT>(account, PRECISION_9);
-        // let token = TokenMock::mint_token<XUSDT>(5000000u128);
-        // coin::deposit(signer::address_of(account), token);
     }
 }
--- aptos/scripts/devnet_check.sh
+++ aptos/scripts/devnet_check.sh
@@ -14,43 +14,43 @@
 
 
 ### 测试账号接收token
-aptos move run --function-id 0xf0b07b5181ce76e447632cdff90525c0411fd15eb61df7da4e835cf88dc05f5b::CommonHelper::accept_token_entry --type-args 0xf0b07b5181ce76e447632cdff90525c0411fd15eb61df7da4e835cf88dc05f5b::STAR::STAR --assume-yes --profile  devnet-test
+aptos move run --function-id 0x9bf32e42c442ae2adbc87bc7923610621469bf183266364503a7a434fe9d50ca::CommonHelper::accept_token_entry --type-args 0x9bf32e42c442ae2adbc87bc7923610621469bf183266364503a7a434fe9d50ca::STAR::STAR --assume-yes --profile  devnet-test
 
 ### 给test account 转 STAR，单次转4500个STAR
-aptos move run --function-id 0x1::coin::transfer --type-args 0xf0b07b5181ce76e447632cdff90525c0411fd15eb61df7da4e835cf88dc05f5b::STAR::STAR --args address:0xa1f9519d22512ae8b2347ea448463dc85dd49f07f48f78855fc7e920931c58d9 u64:4500000000000 --profile devnet-default --assume-yes
+aptos move run --function-id 0x1::coin::transfer --type-args 0x9bf32e42c442ae2adbc87bc7923610621469bf183266364503a7a434fe9d50ca::STAR::STAR --args address:0xa1f9519d22512ae8b2347ea448463dc85dd49f07f48f78855fc7e920931c58d9 u64:4500000000000 --profile devnet-default --assume-yes
 
 
 
 ### 触发一次swap交易
-aptos move run --function-id 'devnet-default::TokenSwapScripts::swap_exact_token_for_token' --type-args 0xf0b07b5181ce76e447632cdff90525c0411fd15eb61df7da4e835cf88dc05f5b::STAR::STAR 0x1::aptos_coin::AptosCoin  --args u128:200000000000 u128:100 --assume-yes --profile  devnet-test
+aptos move run --function-id 'devnet-default::TokenSwapScripts::swap_exact_token_for_token' --type-args 0x9bf32e42c442ae2adbc87bc7923610621469bf183266364503a7a434fe9d50ca::STAR::STAR 0x1::aptos_coin::AptosCoin  --args u128:200000000000 u128:100 --assume-yes --profile  devnet-test
 
 ### 再触发一次swap交易，另一个交易对
-aptos move run --function-id 'devnet-default::TokenSwapScripts::swap_exact_token_for_token' --type-args 0xf0b07b5181ce76e447632cdff90525c0411fd15eb61df7da4e835cf88dc05f5b::STAR::STAR 0x1::aptos_coin::AptosCoin  --args u128:100000000000 u128:100 --assume-yes --profile  devnet-test
+aptos move run --function-id 'devnet-default::TokenSwapScripts::swap_exact_token_for_token' --type-args 0x9bf32e42c442ae2adbc87bc7923610621469bf183266364503a7a434fe9d50ca::STAR::STAR 0x1::aptos_coin::AptosCoin  --args u128:100000000000 u128:100 --assume-yes --profile  devnet-test
 
 
 ### 测试添加流动性
-aptos move run --function-id 'devnet-default::TokenSwapScripts::add_liquidity' --type-args 0xf0b07b5181ce76e447632cdff90525c0411fd15eb61df7da4e835cf88dc05f5b::STAR::STAR     0x1::aptos_coin::AptosCoin   --args  u128:1200000000000  u128:300000000  u128:5000  u128:5000  --profile devnet-test --assume-yes
+aptos move run --function-id 'devnet-default::TokenSwapScripts::add_liquidity' --type-args 0x9bf32e42c442ae2adbc87bc7923610621469bf183266364503a7a434fe9d50ca::STAR::STAR     0x1::aptos_coin::AptosCoin   --args  u128:1200000000000  u128:300000000  u128:5000  u128:5000  --profile devnet-test --assume-yes
 
 
 ### 质押流动性
-aptos move run --function-id 'devnet-default::TokenSwapFarmScript::stake' --type-args 0xf0b07b5181ce76e447632cdff90525c0411fd15eb61df7da4e835cf88dc05f5b::STAR::STAR 0x1::aptos_coin::AptosCoin --args  u128:26655101   --profile  devnet-test --assume-yes
+aptos move run --function-id 'devnet-default::TokenSwapFarmScript::stake' --type-args 0x9bf32e42c442ae2adbc87bc7923610621469bf183266364503a7a434fe9d50ca::STAR::STAR 0x1::aptos_coin::AptosCoin --args  u128:26655101   --profile  devnet-test --assume-yes
 
 ### 领取奖励
-aptos move run --function-id 'devnet-default::TokenSwapFarmScript::harvest' --type-args 0xf0b07b5181ce76e447632cdff90525c0411fd15eb61df7da4e835cf88dc05f5b::STAR::STAR 0x1::aptos_coin::AptosCoin --args  u128:0   --profile  devnet-test --assume-yes
+aptos move run --function-id 'devnet-default::TokenSwapFarmScript::harvest' --type-args 0x9bf32e42c442ae2adbc87bc7923610621469bf183266364503a7a434fe9d50ca::STAR::STAR 0x1::aptos_coin::AptosCoin --args  u128:0   --profile  devnet-test --assume-yes
 
 
 
 ### 取出Farm质押
-aptos move run --function-id 'devnet-default::TokenSwapFarmScript::unstake' --type-args 0xf0b07b5181ce76e447632cdff90525c0411fd15eb61df7da4e835cf88dc05f5b::STAR::STAR 0x1::aptos_coin::AptosCoin  --args  u128:3100   --profile  devnet-test --assume-yes
+aptos move run --function-id 'devnet-default::TokenSwapFarmScript::unstake' --type-args 0x9bf32e42c442ae2adbc87bc7923610621469bf183266364503a7a434fe9d50ca::STAR::STAR 0x1::aptos_coin::AptosCoin  --args  u128:3100   --profile  devnet-test --assume-yes
 
 ### 质押Syrup
-aptos move run --function-id 'devnet-default::TokenSwapSyrupScript::stake' --type-args 0xf0b07b5181ce76e447632cdff90525c0411fd15eb61df7da4e835cf88dc05f5b::STAR::STAR  --args  u64:100 u128:12000000000   --profile  devnet-test --assume-yes
+aptos move run --function-id 'devnet-default::TokenSwapSyrupScript::stake' --type-args 0x9bf32e42c442ae2adbc87bc7923610621469bf183266364503a7a434fe9d50ca::STAR::STAR  --args  u64:100 u128:12000000000   --profile  devnet-test --assume-yes
 
-aptos move run --function-id 'devnet-default::TokenSwapSyrupScript::stake' --type-args 0xf0b07b5181ce76e447632cdff90525c0411fd15eb61df7da4e835cf88dc05f5b::STAR::STAR  --args  u64:3600 u128:25000000000   --profile  devnet-test --assume-yes
+aptos move run --function-id 'devnet-default::TokenSwapSyrupScript::stake' --type-args 0x9bf32e42c442ae2adbc87bc7923610621469bf183266364503a7a434fe9d50ca::STAR::STAR  --args  u64:3600 u128:25000000000   --profile  devnet-test --assume-yes
 
 
 ### 给farm池boost加速
-aptos move run --function-id 'devnet-default::TokenSwapFarmScript::boost' --type-args 0xf0b07b5181ce76e447632cdff90525c0411fd15eb61df7da4e835cf88dc05f5b::STAR::STAR 0x1::aptos_coin::AptosCoin  --args  u128:1445965   --profile  devnet-test --assume-yes
+aptos move run --function-id 'devnet-default::TokenSwapFarmScript::boost' --type-args 0x9bf32e42c442ae2adbc87bc7923610621469bf183266364503a7a434fe9d50ca::STAR::STAR 0x1::aptos_coin::AptosCoin  --args  u128:1445965   --profile  devnet-test --assume-yes
 
 ### syrup unstake
-aptos move run --function-id 'devnet-default::TokenSwapSyrupScript::unstake' --type-args 0xf0b07b5181ce76e447632cdff90525c0411fd15eb61df7da4e835cf88dc05f5b::STAR::STAR  --args  u64:1   --profile  devnet-test --assume-yes
+aptos move run --function-id 'devnet-default::TokenSwapSyrupScript::unstake' --type-args 0x9bf32e42c442ae2adbc87bc7923610621469bf183266364503a7a434fe9d50ca::STAR::STAR  --args  u64:1   --profile  devnet-test --assume-yes
